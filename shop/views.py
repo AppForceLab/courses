973d8ff992939dbfb17d4557105d1f459b04f450
Volodymyr Popov <appforcelab@gmail.com>
2023-11-10 15:58:26 +0000

    add routs

--- shop/views.py
+++ shop/views.py
@@ -1,8 +1,20 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404
 from .models import Course, Category
 
 
-def index(reqest):
+def index(request):
     courses = Course.objects.all()
-    return HttpResponse(courses)
+    return render(request, 'shop/courses.html', {"courses": courses})
+
+
+def singe_course(request, course_id):
+    # OPTION1
+    # try:
+    #     course = Course.objects.get(pk=course_id)
+    #     return render(request, 'single_course.html', {"course": course})
+    # except Course.DoesNotExist:
+    #     raise Http404()
+
+    course = get_object_or_404(Course, pk=course_id)
+    return render(request, 'shop/single_course.html', {"course": course})
